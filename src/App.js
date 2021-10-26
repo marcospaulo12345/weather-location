14fabe34e430e784148b2077cb7f5bb276a6e46c
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2021-10-26 14:27:10 -0300

    Inicio da estilização

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
 
+import './App.css';
+
 function App() {
   const [location, setLocation] = useState(false);
   const [weather, setWeather]  = useState(false);
+  const [tempHorly, setTempHourly] = useState(false);
   const [city, setCity] = useState('');
 
   let getWeather = async (lat, long) => {
@@ -17,7 +20,13 @@ function App() {
       }
     });
     setWeather(res.data);
-    console.log(res)
+    setTempHourly(res.data.hourly)
+    //for(var i =0; i<7; i++){
+      //console.log(res.data.daily[String(i)])
+    //}
+    console.log(tempHorly)
+    //console.log(res.data)
+    
 
     let res_city = await axios.get('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=' + lat + '&longitude=' + long + '&localityLanguage=pt')
     setCity(res_city.data['city'])
@@ -45,16 +54,32 @@ function App() {
     )
   }else{
     return (
-      <Fragment>
-        <h3>Clima na cidade de {city}</h3>
-        <hr/>
-        <ul>
-          <li>Temperatura atual: {weather['daily']['0']['temp']['day']}°</li>
-          <li>Temperatura máxima: {weather['daily']['0']['temp']['max']}°</li>
-          <li>Temperatura minima: {weather['daily']['0']['temp']['min']}°</li>
-          <li>Pressão: {weather['daily']['0']['pressure']} hpa</li>
-          <li>Humidade: {weather['daily']['0']['humidity']}%</li>
-        </ul>
+      <Fragment id='App'>
+        <main>
+          <header id='App-header'>
+            <p>{city}</p>
+            <p>26.10.2021</p>
+            <p>Rain map</p>
+          </header>
+          <section id='App-center'>
+            <h1>{parseInt(weather['daily']['0']['temp']['day'])}°</h1>
+
+            <div>
+              <h2>{weather['daily']['0']['weather']['0']['description']}</h2>
+            </div>
+          </section>
+
+          <footer id='App-footer'>
+            { weather['daily'].map(item => (
+              <p>{item['temp']['day']}</p>
+            )) }
+          </footer>
+        </main>
+        <aside>
+          { weather['hourly'].map((item, index) => (
+              <p>{item['temp']}</p>
+            )) }
+        </aside>
     </Fragment>
       );
   }
